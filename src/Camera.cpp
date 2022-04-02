4ab21365246af18c832711992af92365b68bb297
Phillip Cook <yru2kind@gmail.com>
2022-04-02 01:59:18 -0500

    Implemented Camera

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,1 +1,18 @@
 #include "Camera.hpp"
+
+
+Camera::Camera()
+{
+    m_view.setSize(sf::Vector2f(400, 300));
+}
+
+void Camera::setTargetPosition(sf::Vector2f& pos)
+{
+    m_pos = pos;
+    m_view.setCenter(m_pos);
+}
+
+const sf::View& Camera::getView() const
+{
+    return m_view;
+}
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -1,10 +1,15 @@
 #pragma once
+#include "SFML/Graphics.hpp"
 
 class Camera
 {
 
 public:
     Camera();
-
+    void setTargetPosition(sf::Vector2f& pos);
+    const sf::View& getView() const;
 private:
+    sf::View m_view;
+    sf::Vector2f m_pos;
+    sf::Vector2f m_targetPos;
 };
