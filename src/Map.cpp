dab964c9eae0891dcaea0d46b6c0623a7bff1d10
Phillip Cook <yru2kind@gmail.com>
2022-04-02 14:30:08 -0500

    Fixed walls

--- src/Map.cpp
+++ src/Map.cpp
@@ -6,10 +6,14 @@ Map::Map()
     m_wallTexture.loadFromFile("resources/gfx/WoodWall.png");
     m_floorTexture.setRepeated(true);
     m_wallTexture.setRepeated(true);
+    m_floorTexture.setSmooth(true);
+    m_wallTexture.setSmooth(true);
     m_wallSprites.setTexture(m_wallTexture);
-    m_wallSprites.setTextureRect(sf::IntRect(-16,-16,816,616));
+    m_wallSprites.setTextureRect(sf::IntRect(0,0,832,632));
+    m_wallSprites.setOrigin(16,16);
     m_floorSprites.setTexture(m_floorTexture);
     m_floorSprites.setTextureRect(sf::IntRect(0,0,800,600));
+
     
 }
 
