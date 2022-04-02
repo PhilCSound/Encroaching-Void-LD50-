afe88086f3e441d7676657ac1087852a9992fd2b
Phillip Cook <yru2kind@gmail.com>
2022-04-02 03:36:26 -0500

    Added collision with void

--- src/LightMap.cpp
+++ src/LightMap.cpp
@@ -4,7 +4,8 @@ LightMap::LightMap(unsigned int width, unsigned int height)
 {
     m_renderText.create(width, height);
     m_sprite.setTexture(m_renderText.getTexture());
-    m_renderText.clear(sf::Color::White);
+    m_renderText.clear(sf::Color(255, 255, 255, 0));
+    m_image = m_renderText.getTexture().copyToImage();
 }
 
 void LightMap::AddLight(sf::Vector2f& pos, float radias)
@@ -12,13 +13,31 @@ void LightMap::AddLight(sf::Vector2f& pos, float radias)
     sf::CircleShape light(30);
     light.setRadius(radias);
     light.setPosition(pos - sf::Vector2f(radias, radias));
-    light.setFillColor(sf::Color::Yellow);
-    m_renderText.draw(light, sf::BlendAdd);
+    light.setFillColor(sf::Color(68,34,68,240));
+    m_renderText.draw(light, sf::BlendNone);
     m_renderText.display();
 }
 
+bool LightMap::checkCollision(sf::Vector2f pos, int radias)
+{
+    int x = pos.x;
+    int y = pos.y;
+    sf::Color col = m_image.getPixel(x, y); 
+    return (col != sf::Color(255, 255, 255, 0));
+
+}
+
+void LightMap::update()
+{
+    frameCount += 1;
+    if (frameCount < frameToUpdateImage)
+        return;
+    frameCount = 0;
+    m_image = m_renderText.getTexture().copyToImage();
+}
 
 void LightMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     target.draw(m_sprite, states);
 }
+
--- src/LightMap.hpp
+++ src/LightMap.hpp
@@ -7,10 +7,13 @@ public:
     LightMap(unsigned int width = 800, unsigned int height = 600);
     void AddLight(sf::Vector2f& pos, float radias);
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-
+    bool checkCollision(sf::Vector2f pos, int radias);
+    void update();
 private:
     sf::RenderTexture m_renderText;
     sf::Sprite m_sprite;
-
+    sf::Image m_image;
+    int frameCount = 0;
+    int frameToUpdateImage = 12;
 
 };
