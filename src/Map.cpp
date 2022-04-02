46c0b4773fe8fbd2c1453613b12733e4d3a0777a
Phillip Cook <yru2kind@gmail.com>
2022-04-02 17:21:22 -0500

    Collison

--- src/Map.cpp
+++ src/Map.cpp
@@ -13,7 +13,8 @@ Map::Map()
     m_wallSprites.setOrigin(16,16);
     m_floorSprites.setTexture(m_floorTexture);
     m_floorSprites.setTextureRect(sf::IntRect(0,0,800,600));
-
+    //Collison box, kinda hacky, but hey might just work...
+    m_collisionBox = sf::FloatRect(TILE_SIZE, MAP_BOUNDS - TILE_SIZE * 2.0f);
     
 }
 
@@ -21,4 +22,22 @@ void Map::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     target.draw(m_wallSprites, states);
     target.draw(m_floorSprites, states);
+}
+
+void Map::checkBounds(sf::Vector2f& vel, sf::FloatRect& bounds)
+{
+    sf::Vector2f size = sf::Vector2f(bounds.width, bounds.height);
+    sf::Vector2f calcPos = vel + sf::Vector2f(bounds.left, bounds.top);
+    sf::FloatRect tempBounds = sf::FloatRect(calcPos, size);
+
+    if (m_collisionBox.intersects(tempBounds))
+        return;
+    if (calcPos.x <= 0)
+        vel.x = 0 - bounds.left;
+    if (calcPos.x >= MAP_BOUNDS.x)
+        vel.x -= calcPos.x - MAP_BOUNDS.x;
+    if (calcPos.y <= 0)
+        vel.y = 0 - bounds.top;
+    if (calcPos.y >= MAP_BOUNDS.y)
+        vel.y -= calcPos.y - MAP_BOUNDS.y;
 }
--- src/Map.hpp
+++ src/Map.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+#include <iostream>
 
 class Map : public sf::Drawable
 {
@@ -7,10 +8,11 @@ class Map : public sf::Drawable
 public:
     Map();
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
-    
+    void checkBounds(sf::Vector2f& vel, sf::FloatRect& bounds);
 private:
     const sf::Vector2f TILE_SIZE { 16.0f, 16.0f};
-    const sf::Vector2f MAP_BOUNDS { 800.0f, 600.0f};
+    const sf::Vector2f MAP_BOUNDS { 800.0f, 592.0f};
+    sf::FloatRect m_collisionBox;
     sf::Texture m_floorTexture;
     sf::Texture m_wallTexture;
     sf::Sprite m_wallSprites;
