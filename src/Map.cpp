2dba1e0880fa3936fd63f3655d3aeac7becb8169
Phillip Cook <yru2kind@gmail.com>
2022-04-02 01:13:50 -0500

    Added map functionality

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,1 +1,20 @@
 #include "Map.hpp"
+
+Map::Map()
+{
+    m_floorTexture.loadFromFile("resources/gfx/WoodTile.png");
+    m_wallTexture.loadFromFile("resources/gfx/WoodWall.png");
+    m_floorTexture.setRepeated(true);
+    m_wallTexture.setRepeated(true);
+    m_wallSprites.setTexture(m_wallTexture);
+    m_wallSprites.setTextureRect(sf::IntRect(-16,-16,816,616));
+    m_floorSprites.setTexture(m_floorTexture);
+    m_floorSprites.setTextureRect(sf::IntRect(0,0,800,600));
+    
+}
+
+void Map::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(m_wallSprites, states);
+    target.draw(m_floorSprites, states);
+}
--- src/Map.hpp
+++ src/Map.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 
-class Map
+class Map : public sf::Drawable
 {
-public:
-    
 
+public:
+    Map();
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    
 private:
+    const sf::Vector2f TILE_SIZE { 16.0f, 16.0f};
+    const sf::Vector2f MAP_BOUNDS { 800.0f, 600.0f};
+    sf::Texture m_floorTexture;
+    sf::Texture m_wallTexture;
+    sf::Sprite m_wallSprites;
+    sf::Sprite m_floorSprites;
 };
 
