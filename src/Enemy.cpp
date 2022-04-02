3eebb5820c7bb7768f46082dc931467c8ac310f8
Phillip Cook <yru2kind@gmail.com>
2022-04-02 18:14:39 -0500

    Wrote enemy i mean copy and pasted player

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,1 +1,53 @@
 #include "Enemy.hpp"
+
+Enemy::Enemy(sf::Vector2f& pos, sf::Texture& text)
+    : m_pos(pos)
+{
+    m_sprite.setTexture(text);
+    m_sprite.setRotation(m_angle);
+    m_sprite.setPosition(m_pos);
+    m_hitbox = sf::FloatRect(m_pos.x -8, m_pos.y -8, 16.0f,16.0f);
+
+}
+
+void Enemy::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(m_sprite);
+}
+
+
+void Enemy::AddVelocity(sf::Vector2f& velocity)
+{
+    m_vel += velocity;
+}
+
+void Enemy::Move(sf::Vector2f newVel)
+{   
+    m_pos += newVel;
+    m_sprite.setPosition(m_pos);
+    m_hitbox = sf::FloatRect(m_pos - sf::Vector2f(8,8), sf::Vector2f(16, 16));
+}
+
+void Enemy::LookAt(sf::Vector2f& pos)
+{
+    m_angle = atan2(m_pos.y - pos.y, m_pos.x - pos.x) * 180 / 3.141592f - 90.0f;
+    m_sprite.setRotation(m_angle);
+}
+
+sf::Vector2f Enemy::getPosition() const
+{
+    return m_pos;
+}
+
+sf::FloatRect Enemy::getBounds() const
+{
+    return m_hitbox;
+}
+
+sf::Vector2f Enemy::getVelocity() const
+{
+    float invSqrt2 = 0.707106f;
+    if (m_vel.x != 0.0f && m_vel.y != 0.0f)
+        return m_vel * invSqrt2 * m_moveSpeed;
+    return m_vel * m_moveSpeed;
+}
--- src/Enemy.hpp
+++ src/Enemy.hpp
@@ -1,10 +1,24 @@
 #pragma once
+#include "SFML/Graphics.hpp"
 
-class Enemy
+class Enemy : public sf::Drawable
 {
 public:
-    Enemy();
+    Enemy(sf::Vector2f& pos, sf::Texture& text);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    void AddVelocity(sf::Vector2f& velocity);
+    void Move(sf::Vector2f newVel);
+    void LookAt(sf::Vector2f& pos);
+    sf::Vector2f getPosition() const;
+    sf::FloatRect getBounds() const;
+    sf::Vector2f getVelocity() const;
+    
 
 private:
-        
+    sf::Vector2f m_pos;
+    sf::Vector2f m_vel;
+    sf::FloatRect m_hitbox;
+    sf::Sprite m_sprite;
+    float m_angle = 0.0f;
+    float m_moveSpeed = 2.0f;
 };
