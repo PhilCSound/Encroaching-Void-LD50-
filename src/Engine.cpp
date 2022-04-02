196dc2d8c294a9e4d7ab4b55ce92589ea4f1c039
Phillip Cook <yru2kind@gmail.com>
2022-04-02 14:21:27 -0500

    Set Vsync to true

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -4,7 +4,7 @@ Engine::Engine()
 {
 	m_window.create(sf::VideoMode(800, 600, 32), "Encroaching Void");
 	m_window.setFramerateLimit(60);
-	m_window.setVerticalSyncEnabled(false);
+	m_window.setVerticalSyncEnabled(true);
 	m_window.setKeyRepeatEnabled(false);
 	m_isRunning = m_window.isOpen();
 }
