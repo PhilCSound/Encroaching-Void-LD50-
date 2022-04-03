0d08040b2e4170862791b8a590727f51b4662e13
Phillip Cook <yru2kind@gmail.com>
2022-04-02 20:26:19 -0500

    Bullet implement test

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -1,1 +1,28 @@
 #include "Bullet.hpp"
+
+Bullet::Bullet(sf::Vector2f& pos, sf::Vector2f& vel, sf::Texture& text)
+    : m_pos(pos), m_vel(vel)
+{
+    m_sprite.setTexture(text);
+    sf::Vector2f size = sf::Vector2f(text.getSize().x, text.getSize().y) * .5f;
+    m_sprite.setOrigin(size);
+    m_sprite.setPosition(m_pos);
+    m_hitBox = sf::FloatRect(m_pos - size, size * 2.0f);
+    m_halfSize = size;
+}
+
+sf::Vector2f Bullet::getPos()
+{
+    return m_pos;
+}
+
+    
+void Bullet::update(sf::Time elapTime)
+{
+    m_pos += m_vel;
+}
+
+void Bullet::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(m_sprite, states);
+}
--- src/Bullet.hpp
+++ src/Bullet.hpp
@@ -1,8 +1,18 @@
 #pragma once
+#include "SFML/Graphics.hpp"
+#include <cmath>
 
-class Bullet
+class Bullet : public sf::Drawable
 {
 public:
-    Bullet();
+    Bullet(sf::Vector2f& pos, sf::Vector2f& m_vel, sf::Texture& text);
+    void update(sf::Time elapTime);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    sf::Vector2f getPos();
 private:
+    sf::Vector2f m_pos;
+    sf::Vector2f m_vel;
+    sf::Vector2f m_halfSize;
+    sf::FloatRect m_hitBox;
+    sf::Sprite m_sprite;
 };
