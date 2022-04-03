4a2156017fcd6dd96d4c9c1d8951463e242fde82
Phillip Cook <yru2kind@gmail.com>
2022-04-02 19:34:50 -0500

    Added fucntions to follow player within range

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -5,9 +5,19 @@ Enemy::Enemy(sf::Vector2f& pos, sf::Texture& text)
 {
     m_sprite.setTexture(text);
     m_sprite.setRotation(m_angle);
+    m_sprite.setOrigin(sf::Vector2f(text.getSize()) * 0.5f);
     m_sprite.setPosition(m_pos);
     m_hitbox = sf::FloatRect(m_pos.x -8, m_pos.y -8, 16.0f,16.0f);
 
+}
+
+void Enemy::update()
+{
+    ++m_frameCount;
+    if (m_frameCount < m_changeDirFrame)
+        return;
+    m_frameCount = 0;
+    changeDir = true;
 }
 
 void Enemy::draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -18,7 +28,9 @@ void Enemy::draw(sf::RenderTarget& target, sf::RenderStates states) const
 
 void Enemy::AddVelocity(sf::Vector2f& velocity)
 {
-    m_vel += velocity;
+    changeDir = false;
+    m_vel = velocity;
+    LookAt(m_pos + velocity);
 }
 
 void Enemy::Move(sf::Vector2f newVel)
@@ -50,4 +62,18 @@ sf::Vector2f Enemy::getVelocity() const
     if (m_vel.x != 0.0f && m_vel.y != 0.0f)
         return m_vel * invSqrt2 * m_moveSpeed;
     return m_vel * m_moveSpeed;
+}
+
+void Enemy::AttackInRadias(sf::Vector2f& pos)
+{
+    if (pos.x > m_pos.x + ATTACK_RADIAS || pos.x < m_pos.x - ATTACK_RADIAS)
+        return;
+    if (pos.y > m_pos.y + ATTACK_RADIAS || pos.y < m_pos.y - ATTACK_RADIAS)
+        return;
+    sf::Vector2f dist = pos - m_pos;
+    float len = sqrt(dist.x * dist.x + dist.y * dist.y);
+    dist.x /= len;
+    dist.y /= len;
+    //Move in direction.
+    AddVelocity(dist);
 }
--- src/Enemy.hpp
+++ src/Enemy.hpp
@@ -1,24 +1,29 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+#include <cmath>
 
 class Enemy : public sf::Drawable
 {
 public:
     Enemy(sf::Vector2f& pos, sf::Texture& text);
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    void update();
     void AddVelocity(sf::Vector2f& velocity);
     void Move(sf::Vector2f newVel);
     void LookAt(sf::Vector2f& pos);
     sf::Vector2f getPosition() const;
     sf::FloatRect getBounds() const;
     sf::Vector2f getVelocity() const;
-    
-
+    bool changeDir = true;
+    void AttackInRadias(sf::Vector2f& pos);
 private:
     sf::Vector2f m_pos;
     sf::Vector2f m_vel;
     sf::FloatRect m_hitbox;
     sf::Sprite m_sprite;
     float m_angle = 0.0f;
-    float m_moveSpeed = 2.0f;
+    float m_moveSpeed = 0.5f;
+    int m_frameCount = 0;
+    int m_changeDirFrame = 60;
+    const float ATTACK_RADIAS = 100.0f;
 };
